3ec22d799a5ea33b71a5da676278669b9f6aac25
Matthew Weier O'Phinney <matthew@zend.com>
2017-09-20 16:22:25 -0500

    Provides updated license docblocks and CS consistency fixes

--- src/HydratorProviderInterface.php
+++ src/HydratorProviderInterface.php
@@ -1,11 +1,10 @@
 <?php
-
 /**
- * @link      http://github.com/zendframework/zend-hydrator for the canonical source repository
- *
- * @copyright Copyright (c) 2005-2016 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/zendframework/zend-hydrator for the canonical source repository
+ * @copyright Copyright (c) 2017 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   https://github.com/zendframework/zend-hydrator/blob/master/LICENSE.md New BSD License
  */
+
 namespace Zend\Hydrator;
 
 interface HydratorProviderInterface
