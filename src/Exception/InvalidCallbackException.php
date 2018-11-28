b6115d7610ca82929aaf06e4c40f01b1a6e5ca5d
Matthew Weier O'Phinney <matthew@zend.com>
2018-11-28 09:36:39 -0600

    DomainException already implements ExceptionInterface

--- src/Exception/InvalidCallbackException.php
+++ src/Exception/InvalidCallbackException.php
@@ -12,6 +12,6 @@ namespace Zend\Hydrator\Exception;
 /**
  * Invalid callback exception
  */
-class InvalidCallbackException extends DomainException implements ExceptionInterface
+class InvalidCallbackException extends DomainException
 {
 }
