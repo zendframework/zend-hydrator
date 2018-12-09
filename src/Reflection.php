03ef7352527e64a2c1e8ffcd212b6c96c9223cd9
webimpress <contact@webimpress.com>
2018-12-09 21:23:06 +0000

    Trigger user deprecated error on deprecated classes

--- src/Reflection.php
+++ src/Reflection.php
@@ -9,6 +9,17 @@ declare(strict_types=1);
 
 namespace Zend\Hydrator;
 
+use function sprintf;
+use function trigger_error;
+
+use const E_USER_DEPRECATED;
+
+trigger_error(sprintf(
+    'Class %s is deprecated, please use %s instead',
+    Reflection::class,
+    ReflectionHydrator::class
+), E_USER_DEPRECATED);
+
 /**
  * @deprecated since 3.0.0; to be removed in 4.0.0. Use ReflectionHydrator instead.
  */
